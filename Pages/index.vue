9a51a09a251b07ad390548cabf29617079a5c490
darlingson <dmakuwila@gmail.com>
2024-03-28 22:12:58 +0200

    added components folder and the NavBar and WelcomeBanner components

--- Pages/index.vue
+++ Pages/index.vue
@@ -1,9 +1,20 @@
 <template>
     <div>
       <h1>Welcome to the homepage</h1>
+      <NavBar />
+      <WelcomeBanner />
       <AppAlert>
         This is an auto-imported component
       </AppAlert>
     </div>
   </template>
-  
+  <script>
+  import NavBar from '~/components/NavBar.vue'
+  import WelcomeBanner from '~/components/WelcomeBanner.vue'
+  export default {
+    components: {
+      NavBar,
+      WelcomeBanner
+    }
+  }
+  </script>
---
+++ components/WelcomeBanner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="welcome-banner">
+      <h1>Welcome to My Nuxt.js App</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // WelcomeBanner component logic
+  }
+  </script>
+  
+  <style scoped>
+  .welcome-banner {
+    /* Styles for your welcome banner */
+  }
+  </style>
+  
